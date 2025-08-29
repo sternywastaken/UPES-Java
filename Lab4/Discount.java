bfed7f19756182f03653c1ec541254c4aea27d95
sternywastaken <pchsngmhr@gmail.com>
2025-08-29 15:28:54 +0530

    Updated Discount.java File

--- Lab4/Discount.java
+++ Lab4/Discount.java
@@ -6,13 +6,20 @@ public class Discount {
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter price: ");
     int price = sc.nextInt();
+    double discounted_price;
 
     if (price > 1000) {
       System.out.println("20% discount.");
+      discounted_price = price - (0.20 * price);
+      System.out.println("Discounted Price: " + discounted_price);
     } else if (price > 500) {
       System.out.println("10% discount.");
+      discounted_price = price - (0.10 * price);
+      System.out.println("Discounted Price: " + discounted_price);
     } else {
       System.out.println("5% discount.");
+      discounted_price = price - (0.50 * price);
+      System.out.println("Discounted Price: " + discounted_price);
     }
 
     sc.close();
